5bc2471331b378aa3cef4fd9bf1e9cb769266b6f
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-01-06 00:27:35 +0300

    Implement accounts urls

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    # path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     # path('profile/', include('user_profile.urls')),
 ]
 
